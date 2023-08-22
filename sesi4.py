a6e3686f2add1355c476692a71599f4dcf1e3363
DavaFebrian <114384197+DavaFebrian@users.noreply.github.com>
2023-08-22 13:10:01 +0700

    Add files via upload

---
+++ sesi4.py
@@ -0,0 +1,15 @@
+a = input('putra nusa')
+
+b = input('NIVERSITAS NSA PTRA SKABMI')
+
+c = input('SUKABUMI PUTRA NUSA UNIVERSITAS')
+
+d = input('UNPS')
+
+e = input('TAS SAPU BUMI')
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
